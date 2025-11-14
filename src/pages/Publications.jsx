5ff17d1f3b48d072d44507b884b7a2df924ceb26
Arafat <mdalarafatabir@gmail.com>
2025-11-14 17:55:47 +0600

    new commit

--- src/pages/Publications.jsx
+++ src/pages/Publications.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import img from '../assets/portrait_2.jpg';
+import { CgAirplane } from 'react-icons/cg';
 
 const Publications = () => {
   return (
@@ -50,26 +51,45 @@ const Publications = () => {
       </div>
 
       {/* Journel Publications Section */}
-      <div className="bg-white py-8 mt-8">
-        <div className="px-[5%]">
-          <h1 className="text-black text-2xl lg:text-3xl tracking-wide uppercase my-4">
-            Journal Publications
-          </h1>
-          <div className="w-full h-1 bg-red-500 mb-6"></div>
+     <div className="bg-white py-8 mt-8">
+  <div className="px-[5%]">
+    <h1 className="text-black text-2xl sm:text-2xl lg:text-3xl tracking-wide uppercase my-4">
+      Journal Publications
+    </h1>
 
-          <ul className=" text-[16px] text-gray-800">
-            <li>
-              <strong>1. Datta, R., Choi, S., Chowdhary, A., & Park, Y. (2018).</strong> 
-              P4Guard: Designing P4 Based Firewall. <a className="text-orange-500 underline" href="https://doi.org/10.1109/milcom.2018.8599726" target="_blank" rel="noopener noreferrer"> https://doi.org/10.1109/milcom.2018.8599726</a>
-            </li>
-            <li>
-              <strong> 2. Mitra, A., Palit, S., Chaudhuri, B., Kundu, S., Pathak, S., & Datta, R. (2008).</strong> 
-              A New Partial Image Encryption Method for Secured Multimedia Communication. <a className="text-orange-500 underline" href="https://www.researchgate.net/publication/327513663" target="_blank" rel="noopener noreferrer">https://www.researchgate.net/publication/327513663</a>
-            </li>
-            {/* Add more journal publications here */}
-          </ul>
-        </div>
-      </div>
+    {/* Responsive red line */}
+    <div className="h-1 bg-red-500 mb-6 w-24 sm:w-32 md:w-48 lg:w-64"></div>
+
+    <ul className="text-[16px] text-gray-800 space-y-4">
+      <li>
+        <strong>1. Datta, R., Choi, S., Chowdhary, A., & Park, Y. (2018).</strong> 
+        P4Guard: Designing P4 Based Firewall.{" "}
+        <a
+          href="https://doi.org/10.1109/milcom.2018.8599726"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-orange-500 underline hover:text-red-500 transition-colors duration-300 flex items-center"
+        >
+          Go To Page <CgAirplane></CgAirplane>
+        </a>
+      </li>
+
+      <li>
+        <strong>2. Mitra, A., Palit, S., Chaudhuri, B., Kundu, S., Pathak, S., & Datta, R. (2008).</strong> 
+        A New Partial Image Encryption Method for Secured Multimedia Communication.{" "}
+        <a
+          href="https://www.researchgate.net/publication/327513663"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-orange-500 underline hover:text-red-500 transition-colors duration-300 flex items-center"
+        >
+           Go to Page <CgAirplane></CgAirplane>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
+
     </div>
   );
 };
